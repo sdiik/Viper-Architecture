a371a3d1d0930bab42af165a54053036f0cbe559
sdiik <ahmadshiddiq0@gmail.com>
2021-11-25 09:46:30 +0700

    main project

---
+++ Viper-Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper-Architecture
+//
+//  Created by ahmad shiddiq on 25/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
